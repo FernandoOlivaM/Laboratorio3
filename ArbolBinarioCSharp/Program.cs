391c172e5de73faffb28a6e81c3426d912485346
Fernando Oliva <feroliv4z@gmail.com>
2019-03-07 08:12:58 -0600

    Laboratorio 3
    Laboratorio 3

--- ArbolBinarioCSharp/Program.cs
+++ ArbolBinarioCSharp/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArbolBinarioCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ArbolBinarioCSharp {
+    
+    public class Program {
+        static void Main(string[] args) {
+            ABB arbol = new ABB();
+            //se lee el archivo
+            StreamReader leer = new StreamReader(@"../../../ArchivosBase/MOCK_DATA.csv", true);
+            string line = leer.ReadLine();
+            line = leer.ReadLine();
+            string[] separadas = new string [6];
+            while (line != null) {
+                //se vuelve a leer la línea para no tomar en cuenta los encabezados
+                //se separan los campos del medicamento
+                separadas = line.Split(',');
+                arbol.insertarNodo(separadas);
+                line = leer.ReadLine();
+                arbol.mostrarinOrden();
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
         }
     }
 }
